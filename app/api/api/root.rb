59756ee2ab538007b80c466f400d69147464b878
Wen Peng <nleo.wen@gmail.com>
2015-04-10 13:44:45 +0800

    add helper

--- app/api/api/root.rb
+++ app/api/api/root.rb
@@ -1,9 +1,26 @@
 module API
 	class Root < Grape::API
 		prefix 'api'
+
+		before do
+			error!("401 Unauthorized", 401) unless authenticated_user		
+		end
+
+		helpers do
+			def authenticated_user 
+		      byebug
+		      return true if User.find_by_authentication_token(request.headers['Authentication-Token'])
+		      user = User.find_by_username(params[:username])
+		      user && user.authenticate(params[:password])
+		    end
+
+		    def authenticated_token
+		      byebug
+		      user = User.find_by_authentication_token(request.headers['Authentication-Token'])
+		    end
+		end
+
 		mount API::V1::Root #First version
 		error_formatter :json, API::ErrorFormatter
-
-
 	end
 end
--- app/api/api/v1/users.rb
+++ app/api/api/v1/users.rb
@@ -1,40 +1,17 @@
 module API::V1
   class Users < Grape::API
-	version 'v1', using: :path
-	format :json
+  	version 'v1', using: :path
+  	format :json
 
-
-
-      helpers do
-            def authenticated
-                  user = User.find_by_username(params[:username])
-                  user && user.authenticate(params[:password])
-            end
-
-            def authenticated_token
-                  byebug
-                  user = User.find_by_authentication_token(request.headers['Authentication-Token'])
-            end
-      end
-
-	namespace :users do
+  	namespace :users do
       desc "Return all user"
-        before do 
-            error!("401 Unauthorized",401) unless authenticated
-        end
-
         post '/login' do
-          authenticated.authentication_token
+          authenticated_user.authentication_token
         end
-      end
 
-      namespace :users do
-        before do 
-            error!("401 Unauthorized",401) unless authenticated_token
-        end    
         get '/' do
             User.all
         end
-      end
+    end
   end	
 end
